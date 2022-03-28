afa7fb1689e9b423c5523b4cd5cf8f4a6ce849d7
Al Amin <89458590+alamincoder321@users.noreply.github.com>
2022-03-28 17:54:21 +0600

    change now

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -29,7 +29,7 @@
   <div class="col">
     <div class="e-tabs mb-3 px-3">
       <ul class="nav nav-tabs">
-        <li class="nav-item"><a class="nav-link active" href="#">Users</a></li>
+        <li class="nav-item"><a class="nav-link active" href="#">{{__(Users)}}</a></li>
       </ul>
     </div>
 
